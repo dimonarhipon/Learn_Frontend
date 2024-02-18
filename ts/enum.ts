89386a9cad7c99b544ecb9977767ff675de8072e
D_arh <dimonarhipon@mail.ru>
2024-02-18 12:09:26 +0300

    added: enum

---
+++ ts/enum.ts
@@ -0,0 +1,49 @@
+// enum Color {
+// 	Red = '#c100000',
+// 	Blue = '#007ac1',
+// 	Pink = 0xc10050,
+// 	White = 255,
+// }
+// Color.Red
+// Color.Green
+// Color[0]
+// Color[6]
+
+// const enum Language {
+// 	English,
+// 	Spanish,
+// 	Russian
+// }
+// Language.English
+// Language.Tagalog
+// Language[0]
+// Language[6]
+
+
+// const enum Flippable {
+// 	Burger,
+// 	Chair,
+// 	Cup,
+// 	Skateboard,
+// 	Table
+// }
+// function flip(f: Flippable) { return 'flipped it' }
+
+// flip(Flippable.Chair)
+// flip(Flippable.Cup)
+// flip(12)
+
+
+// const enum Flippable2 {
+// 	Burger = 'Burger',
+// 	Chair = 'Chair',
+// 	Cup = 'Cup',
+// 	Skateboard = 'Skateboard',
+// 	Table = 'Table'
+// }
+// function flip2(f: Flippable2) { return 'flipped it' }
+
+
+// flip2(Flippable2.Chair)
+// flip2(Flippable2.Cup)
+// flip2(12)
